2a632d45c9b52c589b142270ab7795d6f3c5f36c
Vincent Bernat <vincent@bernat.im>
2015-01-25 01:05:12 +0100

    webview: compatibility with Android 4.0

--- src/com/deezer/android/dashkiosk/DashboardActivity.java
+++ src/com/deezer/android/dashkiosk/DashboardActivity.java
@@ -50,9 +50,22 @@ public class DashboardActivity extends Activity {
                 public void run() {
                     try {
                         View decorView = getWindow().getDecorView();
-                        decorView.setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                            View.SYSTEM_UI_FLAG_FULLSCREEN);
+                        if(android.os.Build.VERSION.SDK_INT >= 19) {
+                            decorView.setSystemUiVisibility(
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        } else if (android.os.Build.VERSION.SDK_INT >= 16) {
+                            decorView.setSystemUiVisibility(
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_FULLSCREEN);
+                        } else {
+                            decorView.setSystemUiVisibility(
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                        }
                     } finally {
                         handler.postDelayed(this, 20000);
                     }
