985ae3d071c91a9707af8ae0bd49df8433ab3fae
Vincent Bernat <vincent@bernat.im>
2015-08-08 16:02:52 +0200

    webview: don't use a stable layout all the time
    It seems that on Lollilop, this leaves a band at the bottom of the
    screen.

--- src/com/deezer/android/dashkiosk/DashboardActivity.java
+++ src/com/deezer/android/dashkiosk/DashboardActivity.java
@@ -60,12 +60,12 @@ public class DashboardActivity extends Activity {
                                 .getDefaultSharedPreferences(getApplication());
                             Boolean locked = sharedPref.getBoolean("pref_lock_settings", false);
                             if (locked) {
-                                flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                                flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
                             }
                         }
                         if (android.os.Build.VERSION.SDK_INT >= 16) {
-                            flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            flags |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                                 View.SYSTEM_UI_FLAG_FULLSCREEN;
                         } else {
